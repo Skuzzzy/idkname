ad6827eb9dd3517e3bc2d9e92068abce06093b31
Daniel Baird <JamesDanielBaird@gmail.com>
2015-12-12 18:03:10 -0700

    full adder

--- finalhorse.py
+++ finalhorse.py
@@ -38,21 +38,55 @@ class RelationConst:
     def relval(self):
         return self.const_relval
 
-def and_gen(index_one, index_two):
-    return lambda vals: vals[index_one] and vals[index_two]
-def flip_index(index):
-    return lambda vals: not vals[index]
-def xor_gen(index_one, index_two):
-    return lambda vals: ((not vals[index_one]) and vals[index_two]) or (vals[index_one] and (not vals[index_two]))
-def pt_gen(index):
+# def and_gen(index_one, index_two):
+    # return lambda vals: vals[index_one] and vals[index_two]
+# def nand_gen(index_one, index_two):
+    # return lambda vals: not (vals[index_one] and vals[index_two])
+# def or_gen(index_one, index_two):
+    # return lambda vals: vals[index_one] or vals[index_two]
+# def flip_index(index):
+    # return lambda vals: not vals[index]
+# def xor_gen(index_one, index_two):
+    # return lambda vals: ((not vals[index_one]) and vals[index_two]) or (vals[index_one] and (not vals[index_two]))
+# def pt_gen(index):
+    # return lambda vals: vals[index]
+
+# def xor_helper(a,b):
+    # return ((not a) and b) or (a and (not b))
+# def or_helper(a,b):
+    # return a or b
+# def and_helper(a,b):
+    # return a and b
+
+def and_type(a,b):
+    return lambda vals: a(vals) and b(vals)
+def xor_type(a, b):
+    return lambda vals: ((not a(vals)) and b(vals)) or (a(vals) and (not b(vals)))
+def or_type(a, b):
+    return lambda vals: a(vals) or b(vals)
+def inx(index):
     return lambda vals: vals[index]
 
-adder = [pt_gen(0), pt_gen(1), xor_gen(0,1), and_gen(0,1)]
-half_adder = Relation(adder, [True, False, False, False])
+# Full adder
+# A = 0, B = 1, Cin = 2, S, = 3, Cout = 4
+# full_adder = [pt_gen(0), pt_gen(1), pt_gen(2), xor_gen(xor_gen(0, 1), 2), or_gen(and_gen(xor_gen(0,1), 2), and_gen(0,1))]
+full_adder = [inx(0), inx(1), inx(2), xor_type(xor_type(inx(0), inx(1)), inx(2)), or_type(and_type(xor_type(inx(0),inx(1)), inx(2)), and_type(inx(0),inx(1)))]
 
+full_adder_relation = Relation(full_adder, [True, False, True, False, False, False])
 for x in xrange(1):
-    half_adder = half_adder.next_relation()
-    print(str(([x for x in half_adder.vals])))
+    full_adder_relation = full_adder_relation.next_relation()
+    print(str(([x for x in full_adder_relation.vals])))
+
+# adder = [pt_gen(0), pt_gen(1), xor_gen(0,1), and_gen(0,1)]
+# half_adder = Relation(adder, [True, False, False, False])
+
+# for x in xrange(1):
+    # half_adder = half_adder.next_relation()
+    # print(str(([x for x in half_adder.vals])))
+
+
+
+
 
 # simple_relation = [RelationConst(True), flip_index(1), and_gen(0,1)]
 
